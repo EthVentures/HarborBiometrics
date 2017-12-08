ce7d3aa4e13eea91db65095262e3b81fd54b2a14
Aleksandar Velkoski <avelkoski@ltu.edu>
2017-12-08 14:18:22 -0600

    Age/Gender estimation

--- api/v1.0/__init__.py
+++ api/v1.0/__init__.py
@@ -9,6 +9,58 @@ app = Flask(__name__)
 def homepage():
     return 'Flask API running.'
 
+@app.route("/api/v1.0/estimate/age",methods=["POST"])
+def age_estimation():
+    ## Age estimation
+    json_request = request.get_json()
+    if 'query' in json_request.keys():
+        # initialize
+        br = init_brpy(br_loc="/usr/local/lib")
+        br.br_initialize_default()
+        br.br_set_property('algorithm','AgeEstimation')
+        # get image
+        image = BytesIO(b64decode(json_request.get('query'))).read()
+        # write image to disk
+        with open('/tmp/image.bytes','wb') as fl:
+            fl.write(image)
+        # enroll image
+        br.br_enroll('/tmp/image.bytes','/tmp/matrix.csv')
+        # get data
+        lines = open('/tmp/matrix.csv','r').readlines()
+        # result
+        result = float(lines[1].split(',')[1])
+        # finalize
+        br.br_finalize()
+        return jsonify({'mimetype':'application/json','status':200,'request':request.url,'response':[{'score':result}]})
+    else:
+        return jsonify({'error':{'message':'Request must contain a query'},'status':400,'request':request.url})
+
+@app.route("/api/v1.0/estimate/gender",methods=["POST"])
+def gender_estimation():
+    ## Age estimation
+    json_request = request.get_json()
+    if 'query' in json_request.keys():
+        # initialize
+        br = init_brpy(br_loc="/usr/local/lib")
+        br.br_initialize_default()
+        br.br_set_property('algorithm','GenderEstimation')
+        # get image
+        image = BytesIO(b64decode(json_request.get('query'))).read()
+        # write image to disk
+        with open('/tmp/image.bytes','wb') as fl:
+            fl.write(image)
+        # enroll image
+        br.br_enroll('/tmp/image.bytes','/tmp/matrix.csv')
+        # get data
+        lines = open('/tmp/matrix.csv','r').readlines()
+        # result
+        result = 0 if lines[1].split(',')[10]=='Male' else 1
+        # finalize
+        br.br_finalize()
+        return jsonify({'mimetype':'application/json','status':200,'request':request.url,'response':[{'score':result}]})
+    else:
+        return jsonify({'error':{'message':'Request must contain a query'},'status':400,'request':request.url})
+
 @app.route("/api/v1.0/verification",methods=["POST"])
 def verification():
     ## 1:1 verification
@@ -27,10 +79,10 @@ def verification():
         # enroll
         query = br.br_enroll_template(imagetmpl1)
         target = br.br_enroll_template(imagetmpl2)
-        ## score matrix
+        # score matrix
         scoresmat = br.br_compare_template_lists(target, query)
-        ## result
-        result = br.br_get_matrix_output_at(scoresmat, 0, 0)
+        # result
+        result = float(br.br_get_matrix_output_at(scoresmat, 0, 0))
         # clean up templates
         br.br_free_template(imagetmpl1)
         br.br_free_template_list(target)
