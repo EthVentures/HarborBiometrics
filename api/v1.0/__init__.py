c396f17642272d7af18dcb73e24c9aaadc9eb3c2
Aleksandar Velkoski <avelkoski@ltu.edu>
2017-12-18 19:57:38 -0600

    1:N identification

--- api/v1.0/__init__.py
+++ api/v1.0/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from brpy import init_brpy
 from io import BytesIO
 from base64 import b64decode
+from os import walk
 
 app = Flask(__name__)
 
@@ -94,6 +95,54 @@ def verification():
     else:
         return jsonify({'error':{'message':'Request must contain a query and target'},'status':400,'request':request.url})
 
+
+@app.route("/api/v1.0/identification",methods=["POST"])
+def identification():
+    ## 1:N search
+    json_request = request.get_json()
+    if 'query' in json_request.keys():
+        # initialize
+        br = init_brpy(br_loc="/usr/local/lib")
+        br.br_initialize_default()
+        br.br_set_property('algorithm','FaceRecognition')
+        br.br_set_property('enrollAll','true')
+        # get query
+        image = BytesIO(b64decode(json_request.get('query'))).read()
+        # image template
+        imagetmpl1 = br.br_load_img(image, len(image))
+        # enroll
+        query = br.br_enroll_template(imagetmpl1)
+        # get filenames
+        files = [x[2] for x in walk('/images')]
+        # (in)efficient search
+        scores = []
+        for filename in files[0]:
+            # get target
+            image2 = open('/images/'+filename).read()
+            # image template
+            imagetmpl2 = br.br_load_img(image2, len(image2))
+            # enroll
+            target = br.br_enroll_template(imagetmpl2)
+            # score matrix
+            scoresmat = br.br_compare_template_lists(target, query)
+            # result
+            result = float(br.br_get_matrix_output_at(scoresmat, 0, 0))
+            # cache score
+            scores.append({'filename':filename,'result':result})
+        # sort matches
+        scores.sort(key=lambda x: x['result'],reverse=True)
+        # retreive top 5 matches
+        result = scores[:5]
+        # clean up templates
+        br.br_free_template(imagetmpl1)
+        br.br_free_template_list(target)
+        br.br_free_template(imagetmpl2)
+        br.br_free_template_list(query)
+        # finalize
+        br.br_finalize()
+        return jsonify({'mimetype':'application/json','status':200,'request':request.url,'response':[{'scores':result}]})
+    else:
+        return jsonify({'error':{'message':'Request must contain a query'},'status':400,'request':request.url})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
